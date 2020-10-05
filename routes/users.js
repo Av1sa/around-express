10484559cba804c05d0950f7657d064c5b93ebf6
Avisa <manelis1102@gmail.com>
2020-10-05 19:50:24 +0300

    Ready for review 3// Project 12

--- routes/users.js
+++ routes/users.js
@@ -3,34 +3,40 @@ const path = require("path");
 const fs = require("fs");
 
 router.get("/", (req, res) => {
-  fs.readFile(path.join(__dirname, "../data/", "users.json"), (err, data) => {
-    if (err) {
-      res.status(550).send({ message: "Users file not found" });
-      return;
+  fs.readFile(
+    path.join(__dirname, "../", "data/", "users.json"),
+    (err, data) => {
+      if (err) {
+        res.status(500).send({ message: "Users file not found" });
+        return;
+      }
+      res.send(data.toString());
     }
-    res.send(data.toString());
-  });
+  );
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  fs.readFile(path.join(__dirname, "../data/", "users.json"), (err, data) => {
-    if (err) {
-      res.status(550).send({ message: "Users file not found" });
-      return;
-    }
-    let currentUser = null;
-    JSON.parse(data).map((user) => {
-      if (user._id === id) {
-        currentUser = user;
+  fs.readFile(
+    path.join(__dirname, "../", "data/", "users.json"),
+    (err, data) => {
+      if (err) {
+        res.status(500).send({ message: "Users file not found" });
+        return;
       }
-    });
-    if (!currentUser) {
-      res.status(404).send({ message: "User ID not found" });
-      return;
+      let currentUser = null;
+      JSON.parse(data).map((user) => {
+        if (user._id === id) {
+          currentUser = user;
+        }
+      });
+      if (!currentUser) {
+        res.status(404).send({ message: "User ID not found" });
+        return;
+      }
+      res.send(currentUser);
     }
-    res.send(currentUser);
-  });
+  );
 });
 
 module.exports = router;
